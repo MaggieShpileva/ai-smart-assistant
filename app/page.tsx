adf2701d224e5904d072bad1f7105e2fb936ea50
MaggieShpileva <shpileva.rita@gmail.com>
2024-11-02 09:06:53 +0300

    added api

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+'use client'
+
 /**
  * Import: Components
  */
 import { PageTitle } from '@/lib/components/molecula/PageTitle'
 import { WelcomeSection } from '@/lib/components/molecula/WelcomeSection'
+import { BASE_URL } from '@/network'
+import { useEffect } from 'react'
 
 export default function Home() {
+  const getData = async () => {
+    const response = await fetch(`${BASE_URL}/api/test`)
+    console.log('response', response)
+    const result = await response.json()
+    console.log('result', result)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
   return (
     <>
       <PageTitle />
